9d3f5a1d5e15211745f122a050183a821bc58128
subkt2013 <49891514+subkt2013@users.noreply.github.com>
2024-07-06 17:36:50 +0900

    微修正（クオーテーション→テンプレートリテラル、タイポ修正）

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"],variable:'--font-inter' });
 const iBMPlexSerif = IBM_Plex_Serif({
   subsets: ['latin'],
   weight: ['400','700'],
-  variable:'--font-ibm-plex-selif'
+  variable:'--font-ibm-plex-serif'
 })
 
 export const metadata: Metadata = {
@@ -19,14 +19,10 @@ export const metadata: Metadata = {
   }
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({children,}: Readonly<{children: React.ReactNode;}>) {
   return (
     <html lang="en">
-      <body className={'${inter.variable} ${iBMPlexSerif.variable}' }>{children}</body>
+      <body className={`${inter.variable} ${iBMPlexSerif.variable}` }>{children}</body>
     </html>
   );
 }
